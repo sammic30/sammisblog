f4384b8fafeb8cf3cd6d7a2da51c0edab70dd4fd
Sumeet Singh <you@example.com>
2017-03-30 16:14:29 -0700

    First commit

---
+++ post/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='media/')
+    blog_body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def pub_date_neat(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def summary(self):
+        return self.blog_body[:100]
